ac1374fbb2be6053732425c72796b8d852135c44
HuiMun <jlijs0265@gmail.com>
2023-10-12 10:35:38 +0900

    [기능추가] 사용자 정보 변경 페이지 추가

--- src/pages/UserInformation.js
+++ src/pages/UserInformation.js
@@ -1,7 +1,9 @@
 // material-ui
-import { 
+import {
   Typography,
-  Button 
+  Button,
+  Grid,
+  Avatar
 } from '@mui/material';
 
 // project import
@@ -11,17 +13,99 @@ import MainCard from 'components/MainCard';
 
 const UserInformation = () => (
   <>
-    <Typography variant="h2">프로필</Typography>
-    <Button disableElevation color="primary">
-      사용자 정보 변경
-    </Button>
-    <MainCard title="Sample Card">
-      <Typography variant="body2">
-        Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-        minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-        in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-        descent molls anim id est labours.
-      </Typography>
+    <Grid container direction="row" justifyContent="space-between" alignItems="flex-end">
+      <Typography variant="h2">프로필</Typography>
+      <Button variant="contained">
+        사용자 정보 변경
+      </Button>
+    </Grid>
+    <MainCard>
+      <Grid container direction="column" xs={12}>
+        <Grid item container direction="row" xs={12}>
+          <Grid item xs={5}>
+            <Avatar sx={{width:250, height:250}}>프로필 사진</Avatar>
+          </Grid>
+          <Grid item container direction="column" justifyContent="space-around" xs={7}>
+            <Grid item container direction="row">
+              <Grid item xs={3}>
+                <Typography variant="body2">
+                  이름
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="body2">
+                  홍길동
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="body2">
+                  생년월일
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="body2">
+                  2023-10-12
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item container direction="row">
+              <Grid item xs={3}>
+                <Typography variant="body2">
+                  부서
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="body2">
+                  개발팀
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="body2">
+                  직책
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="body2">
+                  연구원
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item container direction="row">
+              <Grid item xs={3}>
+                <Typography variant="body2">
+                  이메일주소
+                </Typography>
+              </Grid>
+              <Grid item xs={9}>
+                <Typography variant="body2">
+                  honggildong@douzon.com
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+
+        </Grid>
+        <Grid item >
+          <Typography variant="body2">
+            전화번호
+          </Typography>
+        </Grid>
+        <Grid item >
+          <Typography variant="body2">
+            010-0000-0000
+          </Typography>
+        </Grid>
+        <Grid item >
+          <Typography variant="body2">
+            주소
+          </Typography>
+        </Grid>
+        <Grid item >
+          <Typography variant="body2">
+            서울특별시 종로구 창경궁로 254
+          </Typography>
+        </Grid>
+      </Grid>
     </MainCard>
   </>
 );
