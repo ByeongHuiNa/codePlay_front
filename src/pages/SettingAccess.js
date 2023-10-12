d796c4a9e593693a75b791af42b7eaffb5bfa94e
HuiMun <jlijs0265@gmail.com>
2023-10-12 17:06:28 +0900

    [기능추가] 관리자 접근관리페이지 추가

--- src/pages/SettingAccess.js
+++ src/pages/SettingAccess.js
@@ -3,18 +3,20 @@ import { Typography } from '@mui/material';
 
 // project import
 import MainCard from 'components/MainCard';
+import InputSeach from 'components/Input/InputSearch';
+import SettingAccessTable from 'components/Table/SettingAccessTable';
 
-// ==============================|| SAMPLE PAGE ||============================== //
+// ==============================|| 관리자 접근관리 PAGE ||============================== //
 
-const UserInformation = () => (
-  <MainCard title="Sample Card">
-    <Typography variant="body2">
-      Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-      minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-      in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-      descent molls anim id est labours.
-    </Typography>
-  </MainCard>
+const SettingAccess = () => (
+  <>
+    <Typography variant="h2">접근관리</Typography>
+    <MainCard>
+      <Typography variant="h4">사용자명으로 검색</Typography>
+      <InputSeach isPersonIcon={true}></InputSeach>
+      <SettingAccessTable />
+    </MainCard>
+  </>
 );
 
-export default UserInformation;
+export default SettingAccess;
