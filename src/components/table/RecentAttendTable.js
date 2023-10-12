773e04c2e6cb7f2c20decb38e0169647b63cf677
yuna77 <mimi1995@naver.com>
2023-10-12 21:34:29 +0900

    [기능추가] 최근출근기록 테이블 컴포넌트 추가

---
+++ src/components/table/RecentAttendTable.js
@@ -0,0 +1,223 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+// material-ui
+import { Box, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+
+
+// project import
+import Dot from 'components/@extended/Dot';
+import { Button } from '../../../node_modules/@mui/material/index';
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc' ? (a, b) => descendingComparator(a, b, orderBy) : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+// ==============================|| ORDER TABLE - HEADER CELL ||============================== //
+
+const headCells = [
+  {
+    id: 'date',
+    align: 'center',
+    disablePadding: false,
+    label: '날짜'
+  },
+  {
+    id: 'startTime',
+    align: 'center',
+    disablePadding: false,
+    label: '출근시간'
+  },
+  {
+    id: 'endTime',
+    align: 'center',
+    disablePadding: false,
+    label: '퇴근시간'
+  },
+  {
+    id: 'status',
+    align: 'center',
+    disablePadding: false,
+    label: '근태상태'
+  },
+  {
+    id: 'updateBtn',
+    align: 'center',
+    disablePadding: false,
+    label: '수정요청'
+  }
+];
+
+// ==============================|| ORDER TABLE - HEADER ||============================== //
+
+function OrderTableHead({ order, orderBy }) {
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.align}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            {headCell.label}
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+OrderTableHead.propTypes = {
+  order: PropTypes.string,
+  orderBy: PropTypes.string
+};
+
+// ==============================|| ORDER TABLE - STATUS ||============================== //
+
+const OrderStatus = ({ status }) => {
+  let color;
+  let title;
+
+  // 0 : 정상출근
+  // 1 : 휴가(연차,반차,공가)
+  // 2 : 지각
+  // 3 : 조퇴(조기퇴근)
+  // 4 : 결근(출근 혹은 퇴근누락))
+  switch (status) {
+    case 0:
+      color = 'success';
+      title = '정상';
+      break;
+    case 1:
+      color = "primary";
+      title = '휴가';
+      break;
+    case 2:
+      color = 'secondary';
+      title = '지각';
+      break;
+    case 3:
+      color = 'warning';
+      title = '조퇴';
+      break;
+    case 4:
+      color = 'error';
+      title = '결근';
+  }
+
+  return (
+    <Stack direction="row" alignItems="center" justifyContent="center" spacing={1}>
+      <Dot color={color} />
+      <Typography>{title}</Typography>
+    </Stack>
+  );
+};
+
+OrderStatus.propTypes = {
+  status: PropTypes.number
+};
+
+// ==============================|| ORDER TABLE ||============================== //
+
+export default function OrderTable() {
+  const [order] = useState('asc');
+  const [orderBy] = useState('trackingNo');
+  const [selected] = useState([]);
+
+  const isSelected = (trackingNo) => selected.indexOf(trackingNo) !== -1;
+
+  function createData(date, startTime, endTime, status) {
+    return { date, startTime, endTime, status };
+  }
+
+  const datas = [
+    createData('2023/10/11', '08:57:10', '18:05:12', 0),
+    createData('2023/10/12', '08:59:26', '18:21:06', 1),
+    createData('2023/10/13', '08:50:13', '18:12:58', 2),
+    createData('2023/10/14', '08:06:35', '18:07:26', 3),
+    createData('2023/10/15', '08:32:57', '18:01:13', 4),
+  ];
+
+  return (
+    <Box>
+      <TableContainer
+        sx={{
+          width: '100%',
+          overflowX: 'auto',
+          position: 'relative',
+          display: 'block',
+          maxWidth: '100%',
+          '& td, & th': { whiteSpace: 'nowrap' }
+        }}
+      >
+        <Table
+          aria-labelledby="tableTitle"
+          sx={{
+            '& .MuiTableCell-root:first-of-type': {
+              pl: 2
+            },
+            '& .MuiTableCell-root:last-of-type': {
+              pr: 3
+            }
+          }}
+        >
+          <OrderTableHead order={order} orderBy={orderBy} />
+          <TableBody>
+            {stableSort(datas, getComparator(order, orderBy)).map((data, index) => {
+              const isItemSelected = isSelected(data.date);
+              const labelId = `enhanced-table-checkbox-${index}`;
+
+              return (
+                <TableRow
+                  hover
+                  role="checkbox"
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  aria-checked={isItemSelected}
+                  tabIndex={-1}
+                  key={data.date}
+                  selected={isItemSelected}
+                >
+                  <TableCell component="th" id={labelId} scope="data" align="center">
+                    {data.date}
+                  </TableCell>
+                  <TableCell align="center">{data.startTime}</TableCell>
+                  <TableCell align="center">{data.endTime}</TableCell>
+                  <TableCell align="center">
+                    <OrderStatus status={data.status} />
+                  </TableCell>
+                  <TableCell align="center">
+                    <Button variant="contained">수정요청</Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+}
