830b651c56854aba4084cbff39f50d54f3662195
HuiMun <jlijs0265@gmail.com>
2023-10-12 14:28:17 +0900

    [업데이트] 사용방법 주석 업데이트

--- src/components/Input/InputSearch.js
+++ src/components/Input/InputSearch.js
@@ -1,7 +1,14 @@
 import SearchIcon from '@mui/icons-material/Search';
 import {IconButton, InputAdornment, TextField } from '@mui/material/index';
+/*
+  input 요소옆에 돋보기 단추가 추가된 InputSearch 공용으로 사용가능한 컴포넌트
+  MUI의 TextField(input)와 InpuAdornment(input내에 icon이나 button추가)를 이용하여 구축됨.
+  prop로는 id값과, onClick 함수만을 받게함.
+  id 기본값은 search로 해당요소 접근시 아래 id를 기본값으로 사용.
+  input id : {search}-input, button id : {search}-button
+*/
 
-const InputSeach = ({id, onClick}) => (
+const InputSearch = ({id, onClick}) => (
 <TextField id={`${id}-input`} type="search" size="normal" margin="normal" InputProps={{
     endAdornment: (
       <InputAdornment position="end">
@@ -16,7 +23,7 @@ const InputSeach = ({id, onClick}) => (
   }} />
 );
 
-InputSeach.defaultProps = {
+InputSearch.defaultProps = {
   id : "search",
 }
-export default InputSeach;
+export default InputSearch;
