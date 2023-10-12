ffef0fc8d4d5ffead3164d407ede0b9cac3b69cc
yuna77 <mimi1995@naver.com>
2023-10-12 21:33:08 +0900

    [기능추가] 근무시간차트 컴포넌트 추가

---
+++ src/components/chart/AttendChart.js
@@ -0,0 +1,74 @@
+import PropTypes from 'prop-types';
+
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+
+import useChart from './useChart';
+import Chart from './chart';
+
+// ----------------------------------------------------------------------
+
+export default function AttendChart({ chart, ...other }) {
+    const { labels, colors, series, options } = chart;
+
+    const chartOptions = useChart({
+        colors,
+        plotOptions: {
+            bar: {
+                borderRadius : 0,
+                horizontal: false,
+                columnWidth: '45%',
+            },
+        },
+        fill: {
+            type: series.map((i) => i.fill),
+        },
+        stroke:{
+            width: 1,
+        },
+        labels,
+        xaxis: {
+            type: 'string',
+        },
+        yaxis: {
+            min: 0,
+            max: 12,
+            tickAmount: 12,
+        },
+        tooltip: {
+            shared: true,
+            intersect: false,
+            
+            y: {
+                formatter: (value) => {
+                    if (typeof value !== 'undefined') {
+                        return `${value.toFixed(0)} hours`;
+                    }
+                    return value;
+                },
+            },
+        },
+        chart: {
+            type: 'bar',
+            stacked: true,
+          },
+        ...options,
+    });
+
+    return (
+        <Card {...other} sx={{ boxShadow: 0 }}>
+            <Box sx={{ p: 0 }}>
+                <Chart
+                    series={series}
+                    options={chartOptions}
+                    width="100%"
+                    height={270}
+                />
+            </Box>
+        </Card>
+    );
+}
+
+AttendChart.propTypes = {
+    chart: PropTypes.object
+};
