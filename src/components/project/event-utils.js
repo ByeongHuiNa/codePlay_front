afd29252ca60f670da1cd03e151480ace315b6b6
jj <wkdlwps21@naver.com>
2023-10-13 18:00:21 +0900

    [코드변경]풀켈린더 코드 변경

--- src/components/project/event-utils.js
+++ src/components/project/event-utils.js
@@ -1,5 +1,5 @@
-let eventGuid = 0
-let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+let eventGuid = 0;
+let todayStr = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
 
 export const INITIAL_EVENTS = [
   {
@@ -13,26 +13,26 @@ export const INITIAL_EVENTS = [
     start: todayStr + 'T12:00:00'
   },
   {
-    title: 'Meeting', 
+    title: 'Meeting',
     start: new Date(),
     color: 'black',
     textColor: 'yellow'
   },
   {
-      title: 'Meeting',
-      start: '2023-08-12T14:30:00',
-      extendedProps: {
-        status: 'done'
-      }
-    },
-    {
-      title: 'Birthday Party',
-      start: '2023-08-13T07:00:00',
-      backgroundColor: 'green',
-      borderColor: 'green'
-    },
-]
+    title: 'Meeting',
+    start: '2023-08-12T14:30:00',
+    extendedProps: {
+      status: 'done'
+    }
+  },
+  {
+    title: 'Birthday Party',
+    start: '2023-08-13T07:00:00',
+    backgroundColor: 'green',
+    borderColor: 'green'
+  }
+];
 
 export function createEventId() {
-  return String(eventGuid++)
+  return String(eventGuid++);
 }
