4747ba72f977d19b46db7357830cc52c8a99e9ee
nbh7304 <nbh7304@gmail.com>
2023-10-16 10:54:03 +0900

    [기능추가]부서별 사원 휴가보유 현황 테이블 추가

--- src/pages/SeeAllAttendance.js
+++ src/pages/SeeAllAttendance.js
@@ -2,6 +2,7 @@ import { Typography, Box, Grid, Tab, Tabs } from '@mui/material';
 import ComponentSkeleton from './components-overview/ComponentSkeleton';
 import { FormControl, IconButton, InputLabel, MenuItem, TextField } from '../../node_modules/@mui/material/index';
 import Select from '@mui/material/Select';
+import VacationCountTable from 'components/Table/VacationCountTable';
 
 import BasicTab from 'components/tab/BasicTab';
 import SearchIcon from '@mui/icons-material/Search';
@@ -35,18 +36,16 @@ const SeeAllAttendance = () => {
       </Box>
 
       <BasicTab value={value} index={0}>
-        <Grid item xs={12} sx={{ mb: 5 }}>
-          <Typography variant="h5">휴가현황조회페이지</Typography>
-        </Grid>
+        
         <Grid item xs={12} sm={6} md={5} lg={7}>
-        <MainCard title={<Typography variant="h5" sx={{ textAlign: 'center' }}>00부서 휴가보유 현황</Typography>}>
+        <MainCard title={<Typography variant="h5" sx={{ textAlign: 'center' }}>{dept ? `${dept} 부서 휴가보유 현황` : '00부서 휴가보유 현황'}</Typography>}>
 
-            <FormControl sx={{ minWidth: 100 }}>
+            <FormControl sx={{ minWidth: 100, }}>
               <InputLabel id="demo-simple-select-label">부서</InputLabel>
               <Select labelId="demo-simple-select-label" id="demo-simple-select" value={dept} label="month" onChange={handleChange2}>
-                <MenuItem value={10}>개발</MenuItem>
-                <MenuItem value={9}>인사</MenuItem>
-                <MenuItem value={8}>회계</MenuItem>
+                <MenuItem value={'개발'}>개발</MenuItem>
+                <MenuItem value={'인사'}>인사</MenuItem>
+                <MenuItem value={'회계'}>회계</MenuItem>
               </Select>
             </FormControl>
             
@@ -55,6 +54,7 @@ const SeeAllAttendance = () => {
             <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
               <SearchIcon />
             </IconButton>
+            <VacationCountTable />
           </MainCard>
         </Grid>
       </BasicTab>
