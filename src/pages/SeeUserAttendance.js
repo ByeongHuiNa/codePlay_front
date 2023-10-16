18bf9744f376fa8c300a9fe2880b7e94184832f0
nbh7304 <nbh7304@gmail.com>
2023-10-16 17:59:08 +0900

    [기능추가]결재대기내역 테이블 추가

--- src/pages/SeeUserAttendance.js
+++ src/pages/SeeUserAttendance.js
@@ -1,23 +1,18 @@
 import { Typography, Box, Grid, Tab, Tabs } from '@mui/material';
 import ComponentSkeleton from './components-overview/ComponentSkeleton';
 
-
-
-
-
 import BasicTab from 'components/tab/BasicTab';
-
-
 
 import MainCard from 'components/MainCard';
 import { useState } from 'react';
+import { Avatar } from '../../node_modules/@mui/material/index';
+
+import ApprovalWaitTable from 'components/Table/ApprovalWaitTable';
+
 
 const SeeUserAttendance = () => {
- 
   const [value, setValue] = useState(0);
   const [value2, setValue2] = useState(0);
-
- 
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -26,9 +21,6 @@ const SeeUserAttendance = () => {
   const handleChange3 = (event, newValue) => {
     setValue2(newValue);
   };
-    
- 
-  
 
   return (
     <ComponentSkeleton>
@@ -41,39 +33,87 @@ const SeeUserAttendance = () => {
 
       <BasicTab value={value} index={0}>
         <Grid item xs={12} sm={6} md={5} lg={7}>
-          <MainCard
-            title={
-              <Typography variant="h5" sx={{ textAlign: 'center' }}>
-                
-              </Typography>
-            }
-          >
-            
-
-            
+          <MainCard>
+            <Grid container direction="column" xs={12}>
+              <Grid item container direction="row" xs={12}>
+                <Grid item xs={3}>
+                  <Avatar sx={{ width: 200, height: 200 }}>프로필 사진</Avatar>
+                </Grid>
+                <Grid item container direction="column" justifyContent="space-around" xs={4}>
+                  <Grid item container direction="row">
+                    <Grid item xs={3}>
+                      <Typography variant="body2">부서</Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <Typography variant="body2">개발팀</Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid item container direction="row">
+                    <Grid item xs={3}>
+                      <Typography variant="body2">직책</Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <Typography variant="body2">연구원</Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid item container direction="row">
+                    <Grid item xs={3}>
+                      <Typography variant="body2">이름</Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <Typography variant="body2">홍길동</Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Grid item container direction="column" xs={5}>
+                  <MainCard title="결재대기내역">
+                    <ApprovalWaitTable/>
+                    
+                    
+                  </MainCard>
+                </Grid>
+              </Grid>
+            </Grid>
           </MainCard>
         </Grid>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value2} onChange={handleChange3} aria-label="basic tabs example">
+
+            <Tab label="승인" />
+            <Tab label="반려" />
+          </Tabs>
+        </Box>
+        <BasicTab value={value2} index={0}>
+          <Grid item xs={12}>
+            <MainCard>
+
+            </MainCard>
+          </Grid>
+        </BasicTab>
+        <BasicTab value={value2} index={1}>
+          <Grid item xs={12}>
+            <MainCard>
+
+            </MainCard>
+          </Grid>
+        </BasicTab>
       </BasicTab>
 
       <BasicTab value={value} index={1}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <Tabs value={value2} onChange={handleChange3} aria-label="basic tabs example">
-            <Tab label="일별" />
-            <Tab label="주간" />
+            <Tab label="승인" />
+            <Tab label="반려" />
           </Tabs>
         </Box>
         <BasicTab value={value2} index={0}>
           <Grid item xs={12} sm={6} md={5} lg={7}>
-            <MainCard>
-              
-            </MainCard>
+            <MainCard>zz</MainCard>
           </Grid>
         </BasicTab>
         <BasicTab value={value2} index={1}>
-        <Grid item xs={12} sm={6} md={5} lg={7}>
-            <MainCard>
-              
-            </MainCard>
+          <Grid item xs={12} sm={6} md={5} lg={7}>
+            <MainCard></MainCard>
           </Grid>
         </BasicTab>
       </BasicTab>
