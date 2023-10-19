0dd931dd55f2187fc4569f2794bcaa4999caf28a
yuna77 <mimi1995@naver.com>
2023-10-19 09:08:30 +0900

    [코드변경] DatePicker 코드 변경

--- src/components/DatePicker/BasicDatePicker.js
+++ src/components/DatePicker/BasicDatePicker.js
@@ -1,11 +1,33 @@
+import * as React from 'react';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
-export default function BasicDatePicker() {
+export default function BasicDatePicker({ label, setDate }) {
+  const datePickerFormat = 'YYYY-MM-DD';
+  const datePickerUtils = {
+    format: datePickerFormat,
+    parse: (value) => Dayjs(value, datePickerFormat, true).toDate()
+  };
+
+  const startDateChange = (date) => {
+    setDate(date.format(datePickerFormat));
+  };
+
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker />
+    <LocalizationProvider dateAdapter={AdapterDayjs} dateFormats={datePickerUtils}>
+      <DatePicker
+        label={label}
+        slotProps={{
+          textField: {
+            size: 'medium'
+          }
+        }}
+        format="YYYY / MM / DD"
+        onChange={(newValue) => {
+          startDateChange(newValue);
+        }}
+      />
     </LocalizationProvider>
   );
 }
