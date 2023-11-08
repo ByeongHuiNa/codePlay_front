3521a39cb481a247bc9f187568262dfaea8287ad
nbh7304 <nbh7304@gmail.com>
2023-11-08 17:06:19 +0900

    [코드삭제]사용자별근태현황조회 메뉴 삭제

--- src/menu-items/attendanceManagerMenu.js
+++ src/menu-items/attendanceManagerMenu.js
@@ -42,14 +42,6 @@ const attendanceManagerMenu = {
       breadcrumbs: false
     },
     {
-      id: 'seeUserAttendance',
-      title: '사용자별근태현황조회(담당자)',
-      type: 'item',
-      url: '/seeUserAttendance',
-      icon: icons.PieChartOutlined,
-      breadcrumbs: false
-    },
-    {
       id: 'approvalAttendance',
       title: '전자결재',
       type: 'item',
