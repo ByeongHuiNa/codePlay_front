0024bf26e83f08aa733f2dd7a0fa65bf91dd86e9
HuiMun <jlijs0265@gmail.com>
2023-11-16 18:03:38 +0900

    [코드변경] width 속성 추가

--- src/components/Chip/BasicChip.js
+++ src/components/Chip/BasicChip.js
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types';
 import { Chip } from '../../../node_modules/@mui/material/index';
 
-const BasicChip = ({ label, color }) => {
+const BasicChip = ({ label, color, width }) => {
   return (
     <Chip
       label={label}
       sx={{
         backgroundColor: color,
         color: 'white',
-        width: '110px',
+        width: width,
         height: '35px',
         marginRight: '10px',
         borderRadius: '0px'
@@ -19,7 +19,12 @@ const BasicChip = ({ label, color }) => {
 
 BasicChip.propTypes = {
   label: PropTypes.string,
-  color: PropTypes.string
+  color: PropTypes.string,
+  width: PropTypes.string
+};
+
+BasicChip.defaultProps = {
+  width: '110px'
 };
 
 export default BasicChip;
